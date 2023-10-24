ecbde0cd454478adcadd9292b8c4a847975470cf
Mai Le <s86123@bht-berlin.de>
2023-10-24 16:13:31 +0200

    store set up again

--- ProjectManagementApplication/ProjectManagementApplication/settings.py
+++ ProjectManagementApplication/ProjectManagementApplication/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "store.apps.StoreConfig",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "ProjectManagementApplication.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ClothingStore",
+        "USER": "root",
+        "PASSWORD": "#Mai1997",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 

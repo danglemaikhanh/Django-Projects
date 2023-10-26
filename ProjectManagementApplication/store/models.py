20b022905fd1db451190792d2a503c40b2df6d1c
Mai Le <s86123@bht-berlin.de>
2023-10-26 19:30:50 +0200

    done

--- ProjectManagementApplication/store/models.py
+++ ProjectManagementApplication/store/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+
 class Project(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
